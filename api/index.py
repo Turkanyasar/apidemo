7f4ea3ac485d08b3c6ebed371e52c44a15bd20bd
Turkanyasar <turkanaliyevayasar@gmail.com>
2026-04-30 08:31:13 +0000

    commit

--- api/index.py
+++ api/index.py
@@ -5,7 +5,7 @@ from .config import API_KEY
 app = Flask (__name__)
 
 CORS(app, resources = {r"/api/news": {
-    "origins": ["https://turkanyasar.github.io/start/"],
+    "origins": ["https://turkanyasar.github.io"],
     "methods": ["GET"]
 }})
 
